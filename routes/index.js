aee022d098662fbcbb418c57533b0069a132cca5
ebs-uchimura <uchimura@ebisu-do.jp>
2022-01-28 19:15:59 +0900

    better one

--- routes/index.js
+++ routes/index.js
@@ -1,204 +1,204 @@
 /**
  * index.js
  *
- * 機能：メイン実行ファイル
+ * function：main
  **/
 
-// ■モジュール読み込み
-const express = require('express'); // express 設定
-require('dotenv').config(); // dotenv(環境変数) 設定
+// ■ define module
+const express = require('express'); // express
+require('dotenv').config(); // dotenv configuration
 
 const app = express();
-const router = express.Router(); // router 設定
+const router = express.Router(); // router
 
-// ■クラス初期化
-const DB = require('../class/db'); // DB設定
-const db = new DB(process.env.KEY1, process.env.KEY2, process.env.KEY3, process.env.KEY4); // DBインスタンス生成
-const SQL = require('../class/sql'); // SQL設定
-const sql = new SQL(); // SQLインスタンス設定
+// ■ class initialization
+const DB = require('../class/db'); // DB
+const db = new DB(process.env.KEY1, process.env.KEY2, process.env.KEY3, process.env.KEY4); // DB instance
+const SQL = require('../class/sql'); // SQL
+const sql = new SQL(); // SQL
 
-// ■DB関係定数
+// ■ DB limit
 const DB_LIMIT = 20;
 
-// ★トップページアクセス
+// ★ top page
 router.get('/', (req, res, next) => {
-  let times = req.query.times === void 0 ? 0 : req.query.times; // 検索回数
-  allSearch(res, times); // 全検索
+  let times = req.query.times === void 0 ? 0 : req.query.times; // search times
+  allSearch(res, times); // search all
 });
 
-// ★タイトル検索
+// ★ title search
 router.get('/search1', (req, res, next) => {
-  let searchtl = req.query.ori === void 0 ? req.query.title : req.query.ori; // タイトル
-  let times = req.query.times === void 0 ? 0 : req.query.times; // 検索回数
+  let searchtl = req.query.ori === void 0 ? req.query.title : req.query.ori; // title
+  let times = req.query.times === void 0 ? 0 : req.query.times; // search times
   if(searchtl) {
-    ambiguousSearch('title', searchtl, res, 1, times); // あいまい検索
+    ambiguousSearch('title', searchtl, res, 1, times); // fuzzy search
   } else {
-    allSearch(res, times); // 全検索
+    allSearch(res, times); // search all
   }
 });
 
-// ★管理番号検索
+// ★ management no search
 router.get('/search2', (req, res, next) => {
-  let searchno = req.query.ori === void 0 ? req.query.sort_no : req.query.ori; // 管理番号
-  let times = req.query.times === void 0 ? 0 : req.query.times; // 検索回数
+  let searchno = req.query.ori === void 0 ? req.query.sort_no : req.query.ori; // management no
+  let times = req.query.times === void 0 ? 0 : req.query.times; // search times
   if(searchno) {
-		codeSearch(searchno, res, times); // コード検索
+		codeSearch(searchno, res, times); // code search
   } else {
-  	allSearch(res, times); // 全検索
+  	allSearch(res, times); // search all
   }
 });
 
-// ★酒種類検索
+// ★ types search
 router.get('/search3', (req, res, next) => {
-  let searchgr = req.query.ori === void 0 ? Number(req.query.type) : req.query.ori; // 酒種類
-  let times = req.query.times === void 0 ? 0 : req.query.times; // 検索回数
+  let searchgr = req.query.ori === void 0 ? Number(req.query.type) : req.query.ori; // types
+  let times = req.query.times === void 0 ? 0 : req.query.times; // search times
   if(searchgr) {
-    generalSearch('ganre_id', searchgr, res, 3, times); // 通常検索
+    generalSearch('ganre_id', searchgr, res, 3, times); // general search
   }
 });
 
-// 制作年検索
+// ★ year search
 router.get('/search4', (req, res, next) => {
-  let searchyr = req.query.ori === void 0 ? Number(req.query.year) : req.query.ori; // 制作年
-  let times = req.query.times === void 0 ? 0 : req.query.times; // 検索回数
+  let searchyr = req.query.ori === void 0 ? Number(req.query.year) : req.query.ori; // production year
+  let times = req.query.times === void 0 ? 0 : req.query.times; // search times
   if(searchyr) {
-    generalSearch('man_date', searchyr, res, 4, times); // 通常検索
+    generalSearch('man_date', searchyr, res, 4, times); // general search
   }
 });
 
-// ★周年検索
+// ★ anniversary search
 router.get('/search5', (req, res, next) => {
-  let searchan = req.query.ori === void 0 ? Number(req.query.anniversary) : req.query.ori; // 周年
-  let times = req.query.times === void 0 ? 0 : req.query.times; // 検索回数
+  let searchan = req.query.ori === void 0 ? Number(req.query.anniversary) : req.query.ori; // anniversary
+  let times = req.query.times === void 0 ? 0 : req.query.times; // search times
   if(searchan) {
-    generalSearch('anniversary', searchan, res, 5, times); // 通常検索
+    generalSearch('anniversary', searchan, res, 5, times); // general search
   }
 });
 
-// ★定形外検索
+// ★ out of fixed search
 router.get('/search6', (req, res, next) => {
-  let searchfx = req.query.ori === void 0 ? req.query.fixed : req.query.ori; // 定形外
-  let times = req.query.times === void 0 ? 0 : req.query.times; // 検索回数
+  let searchfx = req.query.ori === void 0 ? req.query.fixed : req.query.ori; // out of fixed
+  let times = req.query.times === void 0 ? 0 : req.query.times; // search times
   if(searchfx) {
-    generalSearch('fixed', searchfx, res, 6, times); // 通常検索 
+    generalSearch('fixed', searchfx, res, 6, times); // general search
   }
 });
 
-// ★キーワード検索
+// ★ keyword search
 router.get('/search7', (req, res, next) => {
-  let searchkw = req.query.ori === void 0 ? req.query.keyword : req.query.ori; // キーワード
-  let times = req.query.times === void 0 ? 0 : req.query.times; // 検索回数
+  let searchkw = req.query.ori === void 0 ? req.query.keyword : req.query.ori; // keyword
+  let times = req.query.times === void 0 ? 0 : req.query.times; // search times
   if(searchkw) {
-  	ambiguousSearch('keyword', searchkw, res, 7, times); // あいまい検索
+  	ambiguousSearch('keyword', searchkw, res, 7, times); // fuzzy search
   } else {
-		allSearch(res, times); // 全検索
+		allSearch(res, times); // search all
   } 
 });
 
-// ★エラー処理
+// ★ error
 router.get('/err', (req, res, next) => {
-  res.render('error', {err: "no data", msg: "検索結果がありませんでした。"}); // レンダリング
+  res.render('error', {err: "no data", msg: "no result"});
 });
 
 
-// ◆1秒後に実行関数（DBアクセス待ち）
+// ◆ execute after 1 second
 const resolveAfter1Seconds = () => {
   return new Promise((resolve) => {
     setTimeout(() => {
-      resolve(sql.getValue); // 値戻し
+      resolve(sql.getValue); // return value
     }, 1000);
   });
 }
 
-// ◆管理番号用データ整形
+// ◆ management no shaping
 const dataShaping = (obj) => {
-  const dataArr = JSON.parse(JSON.stringify((new Array(10000)).fill((new Array(10)).fill(0)))); // 配列定義
-  let tmpArr = []; // ID一時格納用
-  let index = 0; // mapインデックス
+  const dataArr = JSON.parse(JSON.stringify((new Array(10000)).fill((new Array(10)).fill(0)))); // define array
+  let tmpArr = []; // ID set
+  let index = 0; // map index
   Object.keys(obj).map((key, i) => {
-    tmpArr[i] = obj[key].item_id; // ID一時格納
+    tmpArr[i] = obj[key].item_id; // ID set
   });
-  const set = new Set(tmpArr); // 重複無しコレクション
-  const setToArr = Array.from(set); // 配列取得
+  const set = new Set(tmpArr); // no duplicate collection
+  const setToArr = Array.from(set); // get array
   Object.keys(obj).map((key, i) => {
-    index = setToArr.indexOf(obj[key].item_id); // インデックス取得
-    dataArr[index][obj[key].type - 1] = Number(obj[key].no); // 配列に管理番号格納
+    index = setToArr.indexOf(obj[key].item_id); // get index
+    dataArr[index][obj[key].type - 1] = Number(obj[key].no); // set management no into array
   });
-  return dataArr; // 値返し
+  return dataArr; // return value
 }
 
-// ◆全体検索
+// ◆ search all
 const allSearch = (res, times) => {
-  let obj1, obj2; // 結果受取用オブジェクト定義
-  let idArr = []; // ID格納用
+  let obj1, obj2; // result object
+  let idArr = []; // ID array
   (async() => {
-    await sql.doInquiry("SELECT * FROM ?? LIMIT ? OFFSET ?", ['item', DB_LIMIT, DB_LIMIT * times]); // SQLクエリ投げ
-    obj1 = await resolveAfter1Seconds(); // 処理待ち
+    await sql.doInquiry("SELECT * FROM ?? LIMIT ? OFFSET ?", ['item', DB_LIMIT, DB_LIMIT * times]); // SQL query
+    obj1 = await resolveAfter1Seconds(); // wait
     if(obj1 == "error"){
-      await res.redirect("/err"); // エラーの場合トップに帰る
+      await res.redirect("/err"); // if error goto top
     } else {
       await Object.keys(obj1).map((key, i) => {
-        idArr[i] = obj1[key].ID; // ID格納
+        idArr[i] = obj1[key].ID; // ID
       });
-      await sql.doInquiry("SELECT * FROM ?? WHERE ?? IN (?)", ['man_no', 'item_id', idArr]); // SQLクエリ投げ
-      obj2 = await resolveAfter1Seconds(); // 処理待ち
-      await res.render('index', { data: obj1, man_no: dataShaping(obj2), ori: "", no: 0, fix: obj1[0].fixed}); // 画面レンダリング
+      await sql.doInquiry("SELECT * FROM ?? WHERE ?? IN (?)", ['man_no', 'item_id', idArr]); // SQL query
+      obj2 = await resolveAfter1Seconds(); // wait
+      await res.render('index', { data: obj1, man_no: dataShaping(obj2), ori: "", no: 0, fix: obj1[0].fixed}); // render
     }
   })();
 }
 
-// ◆通常検索
+// ◆ general search
 const generalSearch = (column, word, res, no, times) => {
-  let obj1, obj2; // 結果受取用オブジェクト定義
-  let idArr = []; // ID格納用
+  let obj1, obj2; // result object
+  let idArr = []; // ID
   (async() => {
-    await sql.doInquiry("SELECT * FROM ?? WHERE ?? = ? LIMIT ? OFFSET ?", ['item', column, word, DB_LIMIT, DB_LIMIT * times]); // SQLクエリ投げ
-    obj1 = await resolveAfter1Seconds(); // 処理待ち
+    await sql.doInquiry("SELECT * FROM ?? WHERE ?? = ? LIMIT ? OFFSET ?", ['item', column, word, DB_LIMIT, DB_LIMIT * times]); // SQL query
+    obj1 = await resolveAfter1Seconds(); // wait
     if(obj1 == "error"){
-      await res.redirect("/err"); // エラーの場合トップに帰る
+      await res.redirect("/err"); // if error goto top
     } else {
       await Object.keys(obj1).map((key, i) => {
-        idArr[i] = obj1[key].ID; // ID配列に格納
+        idArr[i] = obj1[key].ID; // set ID
       });
-      await sql.doInquiry("SELECT * FROM ?? WHERE ?? IN (?)", ['man_no', 'item_id', idArr]); // SQLクエリ投げ
-      obj2 = await resolveAfter1Seconds(); // 処理待ち
-      await res.render('index', { data: obj1, man_no: dataShaping(obj2), ori: word, no: no, fix: obj1[0].fixed}); // 画面レンダリング
+      await sql.doInquiry("SELECT * FROM ?? WHERE ?? IN (?)", ['man_no', 'item_id', idArr]); // SQL query
+      obj2 = await resolveAfter1Seconds(); // wait
+      await res.render('index', { data: obj1, man_no: dataShaping(obj2), ori: word, no: no, fix: obj1[0].fixed}); // render
     }
   })();
 }
 
-// ◆あいまい検索
+// ◆ fuzzy search
 const ambiguousSearch = (column, word, res, no, times) => {
-  let obj1, obj2; // 結果受取用オブジェクト定義
-  let idArr = []; // ID格納用配列
+  let obj1, obj2; // result object
+  let idArr = []; // ID
   (async() => {
-    await sql.doInquiry("SELECT * FROM ?? WHERE ?? LIKE ? LIMIT ? OFFSET ?", ['item', column, '%' + word + '%', DB_LIMIT, DB_LIMIT * times]); // SQLクエリ投げ
-    obj1 = await resolveAfter1Seconds(); // 処理待ち
+    await sql.doInquiry("SELECT * FROM ?? WHERE ?? LIKE ? LIMIT ? OFFSET ?", ['item', column, '%' + word + '%', DB_LIMIT, DB_LIMIT * times]); // SQL query
+    obj1 = await resolveAfter1Seconds(); // wait
     if(obj1 == "error"){
-      await res.redirect("/err"); // エラーの場合トップに帰る
+      await res.redirect("/err"); // if error goto top
     } else {
       await Object.keys(obj1).map((key, i) => {
-        idArr[i] = obj1[key].ID; // ID格納用
+        idArr[i] = obj1[key].ID; // ID
       });
-      await sql.doInquiry("SELECT * FROM ?? WHERE ?? IN (?)", ['man_no', 'item_id', idArr]); // SQLクエリ投げ
-      obj2 = await resolveAfter1Seconds(); // 処理待ち
-      await res.render('index', { data: obj1, man_no: dataShaping(obj2), ori: word, no: no, fix: obj1[0].fixed}); // 画面レンダリング
+      await sql.doInquiry("SELECT * FROM ?? WHERE ?? IN (?)", ['man_no', 'item_id', idArr]); // SQL query
+      obj2 = await resolveAfter1Seconds(); // wait
+      await res.render('index', { data: obj1, man_no: dataShaping(obj2), ori: word, no: no, fix: obj1[0].fixed}); // render
     }
   })();
 }
 
-// ◆管理番号検索
+// ◆ management no search
 const codeSearch = (word, res, times) => {
-  let obj1, obj2; // 結果受取用オブジェクト定義
+  let obj1, obj2; // result object
   (async() => {
-    await sql.doInquiry("SELECT * FROM ?? LEFT OUTER JOIN ?? ON ?? = ?? WHERE ?? = ? LIMIT ? OFFSET ?", ['item', 'man_no', 'item.ID', 'man_no.item_id', 'man_no.no', word, DB_LIMIT, DB_LIMIT * times]); // SQLクエリ投げ
-    obj1 = await resolveAfter1Seconds(); // 処理待ち
+    await sql.doInquiry("SELECT * FROM ?? LEFT OUTER JOIN ?? ON ?? = ?? WHERE ?? = ? LIMIT ? OFFSET ?", ['item', 'man_no', 'item.ID', 'man_no.item_id', 'man_no.no', word, DB_LIMIT, DB_LIMIT * times]); // SQL query
+    obj1 = await resolveAfter1Seconds(); // wait
     if(obj1 == "error"){
-      await res.redirect("/err"); // エラーの場合トップに帰る
+      await res.redirect("/err"); // if error goto top
     } else {
-      await sql.doInquiry("SELECT * FROM ?? WHERE ?? = ?", ['man_no', 'item_id', obj1[0].ID]); // SQLクエリ投げ
-      obj2 = await resolveAfter1Seconds(); // 処理待ち
-      await res.render('index', { data: obj1, man_no: dataShaping(obj2), ori: word, no: 2, fix: obj1[0].fixed}); // 画面レンダリング
+      await sql.doInquiry("SELECT * FROM ?? WHERE ?? = ?", ['man_no', 'item_id', obj1[0].ID]); // SQL query
+      obj2 = await resolveAfter1Seconds(); // wait
+      await res.render('index', { data: obj1, man_no: dataShaping(obj2), ori: word, no: 2, fix: obj1[0].fixed}); // render
     }
   })();
 }
